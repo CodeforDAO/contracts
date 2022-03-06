5984c656feb31844c0f59da8bf33c2de387c7dd8
guoyu <o.u.turing@gmail.com>
2022-03-07 02:58:26 +0900

    Improve test cases

--- test/membership.js
+++ test/membership.js
@@ -14,15 +14,15 @@ describe("Membership", function () {
     // Create a test merkle tree
     const leafNodes = (await Promise.all(accounts.filter((_, idx) => idx < 4)
       .map(account => account.getAddress()))).map(adr => keccak256(adr));
-
     const merkleTree = new MerkleTree(leafNodes, keccak256, { 
       sortPairs: true, 
-      hashLeaves: true 
     });
     rootHash = merkleTree.getHexRoot()
     proof = merkleTree.getHexProof(keccak256(await owner.getAddress()))
     badProof = merkleTree.getHexProof(keccak256(await accounts[4].getAddress()))
+  })
 
+  beforeEach(async function () {
     // Deploy Membership contract
     Membership = await ethers.getContractFactory("Membership")
     Governor = await ethers.getContractFactory("MembershipGovernor")
@@ -63,6 +63,46 @@ describe("Membership", function () {
         await membership.connect(accounts[1]).updateRoot(rootHash)
       } catch (err) {
         expect(err.message).to.have.string('CodeforDAO Membership: must have inviter role to update root')
+      }
+    })
+  })
+
+  describe("#mint", function () {
+    it("Should able to mint NFT for account in whitelist", async function () {
+      await membership.updateRoot(rootHash)
+      await membership.mint(proof)
+
+      expect(await membership.balanceOf(owner.getAddress())).to.equal(1)
+    })
+
+    it("Should not able to mint NFT for an account more than once", async function () {
+      await membership.updateRoot(rootHash)
+      await membership.mint(proof)
+
+      try {
+        await membership.mint(proof)
+      } catch (err) {
+        expect(err.message).to.have.string('CodeforDAO Membership: address already claimed')
+      }
+    })
+
+    it("Should not able to mint NFT for account in whitelist with badProof", async function () {
+      await membership.updateRoot(rootHash)
+
+      try {
+        await membership.mint(badProof)
+      } catch (err) {
+        expect(err.message).to.have.string('CodeforDAO Membership: Invalid proof')
+      }
+    })
+
+    it("Should not able to mint NFT for account not in whitelist", async function () {
+      await membership.updateRoot(rootHash)
+
+      try {
+        await membership.connect(accounts[4]).mint(badProof)
+      } catch (err) {
+        expect(err.message).to.have.string('CodeforDAO Membership: Invalid proof')
       }
     })
   })
