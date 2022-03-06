aa74f5982870f4d5c7342ccc256726f9d1bb9783
guoyu <o.u.turing@gmail.com>
2022-03-06 23:14:23 +0900

    Rewrite simple deploy script

--- scripts/membership.js
+++ scripts/membership.js
@@ -1,7 +1,7 @@
 const hre = require("hardhat");
 
 async function main() {
-  const contract = await hre.ethers.getContractFactory("TokenClaim");
+  const contract = await hre.ethers.getContractFactory("Membership");
   const _contract = await contract.deploy();
 
   await _contract.deployed();
