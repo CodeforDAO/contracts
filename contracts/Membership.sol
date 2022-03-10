6ac2161e88818b41842e022b93f9c268b84f8b6b
guoyu <o.u.turing@gmail.com>
2022-03-11 03:31:20 +0900

    Add shareToken and DAOSettings to contract membership

--- contracts/Membership.sol
+++ contracts/Membership.sol
@@ -13,12 +13,15 @@ import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/utils/Multicall.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
-import "@openzeppelin/contracts/access/IAccessControl.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 
+// Core contracts of CodeforDAO
 import "./Treasury.sol";
 import "./Governor.sol";
+import "./Share.sol";
 
 contract Membership is 
+  Context,
   AccessControlEnumerable,
   ERC721Enumerable, 
   ERC721Burnable, 
@@ -29,23 +32,48 @@ contract Membership is
   using Counters for Counters.Counter;
   using Strings for uint256;
 
+  struct ShareToken {
+    string name;
+    string symbol;
+    uint256 initialSupply;
+  }
+
+  struct DAOSettings {
+    bool enableMembershipTransfer;
+    uint256 votingDelay;
+    uint256 votingPeriod;
+    uint256 shareGovernorProposalThreshold;
+    uint256 quorumNumerator;
+    uint256 shareGovernorQuorumNumerator;
+  }
+
   bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
   bytes32 public constant INVITER_ROLE = keccak256("INVITER_ROLE");
   bytes32 public merkleTreeRoot;
   mapping(uint256 => string) public useDecentralizedStorage;
+  mapping(uint256 => bool) public isInvestor;
+  DAOSettings public initialSettings;
 
   // Governance related contracts
-  MembershipGovernor public immutable governor;
+  Treasury public immutable treasury;
+  TreasuryGovernor public immutable governor;
+  Share public immutable shareToken;
+  TreasuryGovernor public shareGovernor;
 
   Counters.Counter private _tokenIdTracker;
   string private _baseTokenURI;
+  uint256 private _shareTokenInitialSupply;
 
   constructor(
     string memory name,
     string memory symbol,
-    string memory baseTokenURI
+    string memory baseTokenURI,
+    ShareToken memory shareToken_,
+    DAOSettings memory settings_
   ) ERC721(name, symbol) EIP712(name, "1") {
     _baseTokenURI = baseTokenURI;
+    _shareTokenInitialSupply = shareToken_.initialSupply;
+    initialSettings = settings_;
 
     _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
     _grantRole(PAUSER_ROLE, _msgSender());
@@ -55,24 +83,72 @@ contract Membership is
     address[] memory _executors = new address[](1);
     _executors[0] = address(0);
 
-    governor = new MembershipGovernor({
-      name_: "MembershipGovernor",
+    // Create DAO's Treasury contract
+    treasury = new Treasury(6575, _proposers, _executors);
+
+    // Create DAO's 1/1 Membership Governance contract
+    governor = new TreasuryGovernor({
+      name_: string(abi.encodePacked(name, "MembershipGovernor")),
       token_: this,
-      votingDelay_: 0,
-      votingPeriod_: 46027,
+      votingDelay_: initialSettings.votingDelay,
+      votingPeriod_: initialSettings.votingPeriod,
       proposalThreshold_: 1,
-      quorumNumerator_: 3,
-      treasury_: new Treasury(6575, _proposers, _executors)
+      quorumNumerator_: initialSettings.quorumNumerator,
+      treasury_: treasury 
+    });
+
+    // Create DAO's share token
+    shareToken = new Share(
+      bytes(shareToken_.name).length > 0 ?
+        shareToken_.name :
+        string(abi.encodePacked(name, "Share")),
+      bytes(shareToken_.symbol).length > 0 ?
+        shareToken_.symbol :
+        string(abi.encodePacked(symbol, "SHR"))
+    );
+
+    // Create DAO's share Governance
+    shareGovernor = new TreasuryGovernor({
+      name_: string(abi.encodePacked(name, "ShareGovernor")),
+      token_: shareToken,
+      votingDelay_: initialSettings.votingDelay,
+      votingPeriod_: initialSettings.votingPeriod,
+      proposalThreshold_: initialSettings.shareGovernorProposalThreshold,
+      quorumNumerator_: initialSettings.shareGovernorQuorumNumerator,
+      treasury_: treasury
     });
   }
 
   function setupGovernor() public onlyRole(DEFAULT_ADMIN_ROLE) {
-    IAccessControl treasury = IAccessControl(governor.timelock());
-    treasury.grantRole(keccak256("PROPOSER_ROLE"), address(governor));
+    bytes32 PROPOSER_ROLE = keccak256("PROPOSER_ROLE");
+    bytes32 MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    // Setup governor roles
+    // Both membership and share governance have PROPOSER_ROLE by default
+    treasury.grantRole(PROPOSER_ROLE, address(governor));
+    treasury.grantRole(PROPOSER_ROLE, address(shareGovernor));
+
+    // Revoke `TIMELOCK_ADMIN_ROLE` from this deployer
     treasury.revokeRole(keccak256("TIMELOCK_ADMIN_ROLE"), address(this));
 
+    // Mint initial tokens to the treasury 
+    if (_shareTokenInitialSupply > 0) {
+      shareToken.mint(address(treasury), _shareTokenInitialSupply);
+    }
+
+    // Make sure the DAO's Treasury contract controls everything
     grantRole(DEFAULT_ADMIN_ROLE, address(treasury));
     revokeRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    shareToken.grantRole(DEFAULT_ADMIN_ROLE, address(treasury));
+    shareToken.grantRole(MINTER_ROLE, address(treasury));
+    shareToken.grantRole(PAUSER_ROLE, address(treasury));
+    shareToken.revokeRole(MINTER_ROLE, address(this));
+    shareToken.revokeRole(PAUSER_ROLE, address(this));
+    shareToken.revokeRole(DEFAULT_ADMIN_ROLE, address(this));
+
+    // All membership NFT is set to be non-transferable by default,
+    pause();
+    revokeRole(PAUSER_ROLE, _msgSender());
   }
 
   function tokenURI(uint256 tokenId) public view override returns (string memory) {
@@ -92,6 +168,7 @@ contract Membership is
     return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
   }
 
+  // Self-mint for white-listed members
   function mint(bytes32[] calldata proof) public {
     require(balanceOf(_msgSender()) < 1, "CodeforDAO Membership: address already claimed");
     require(MerkleProof.verify(proof, merkleTreeRoot, keccak256(abi.encodePacked(_msgSender()))), "CodeforDAO Membership: Invalid proof");
@@ -101,6 +178,22 @@ contract Membership is
     _tokenIdTracker.increment();
   }
 
+  // Treasury could mint for a investor by pass the whitelist check
+  function investMint(address to) external onlyRole(DEFAULT_ADMIN_ROLE) returns (uint256) {
+    if (balanceOf(to) > 0) {
+      uint256 tokenId = tokenOfOwnerByIndex(to, 0);
+      isInvestor[tokenId] = true;
+      return tokenId;
+    }
+
+    uint256 _tokenId = _tokenIdTracker.current();
+    _mint(to, _tokenId);
+    isInvestor[_tokenId] = true;
+    _tokenIdTracker.increment();
+    return _tokenId;
+  }
+
+  // Switch for the use of decentralized storage
   function updateTokenURI(uint256 tokenId, string calldata dataURI) public {
     require(_exists(tokenId), "CodeforDAO Membership: URI update for nonexistent token");
     require(ownerOf(tokenId) == _msgSender(), "CodeforDAO Membership: URI update for token not owned by sender");
@@ -108,10 +201,10 @@ contract Membership is
     useDecentralizedStorage[tokenId] = dataURI;
   }
 
-  function updateRoot(bytes32 root) public {
+  function updateWhitelist(bytes32 merkleTreeRoot_) public {
     require(hasRole(INVITER_ROLE, _msgSender()), "CodeforDAO Membership: must have inviter role to update root"); 
 
-    merkleTreeRoot = root;
+    merkleTreeRoot = merkleTreeRoot_;
   }
 
   function pause() public {
