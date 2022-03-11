cbf77722965b8816dff896472f0cee9fda49063f
guoyu <o.u.turing@gmail.com>
2022-03-11 17:27:34 +0900

    Add new dev dep to remove hardhat console.log

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,20 @@
-require("dotenv").config()
+require('dotenv').config();
 
-const fs = require("fs")
-const { utils } = require("ethers")
-const { isAddress, getAddress, formatUnits, parseUnits } = utils
+const fs = require('fs');
+const { utils } = require('ethers');
+const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 
-require("@nomiclabs/hardhat-waffle")
-require("hardhat-deploy")
-require("@nomiclabs/hardhat-ethers")
-require("hardhat-gas-reporter")
-require("@nomiclabs/hardhat-etherscan")
-require("@tenderly/hardhat-tenderly")
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-etherscan');
+require('hardhat-preprocessor');
+require('hardhat-deploy');
+require('hardhat-gas-reporter');
+require('solidity-coverage');
+require('@tenderly/hardhat-tenderly');
 
 // Hardhat Tasks
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -26,41 +28,34 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 function prepareHardhatConfigs() {
   function relayURLs(network) {
     return {
-      'infura': `https://${network}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-    }
+      infura: `https://${network}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+    };
   }
 
   function getMnenomic() {
-    const filePath = process.env.MNEMONIC_PATH || './mnemonic.txt'
+    const filePath = process.env.MNEMONIC_PATH || './mnemonic.txt';
 
     try {
       return fs.readFileSync(filePath).toString().trim();
     } catch (err) {
-      return ''
+      return '';
     }
   }
 
   function prepareNetworkConfigs() {
-    const currentRelay = process.env.DEFAULT_RELAY || 'infura'
-    const mainnetGwei = 21
-    const ethNetworks = [
-      'mainnet',
-      'rinkeby',
-      'kovan',
-      'ropsten',
-      'goerli',
-    ]
+    const currentRelay = process.env.DEFAULT_RELAY || 'infura';
+    const mainnetGwei = 21;
+    const ethNetworks = ['mainnet', 'rinkeby', 'kovan', 'ropsten', 'goerli'];
     const hardhatLocalConfig = {
-      hardhat: {}
-    }
+      hardhat: {},
+    };
 
     // To use this feature you need to connect to an archive node.
     // At this moment it's hardcoded to alchemy archive code.
-    if (process.env.FORK_MAINNET &&
-      process.env.ALCHEMY_API_KEY) {
+    if (process.env.FORK_MAINNET && process.env.ALCHEMY_API_KEY) {
       hardhatLocalConfig.hardhat.forking = {
         url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      }
+      };
     }
 
     return ethNetworks.reduce((acc, network) => {
@@ -69,27 +64,34 @@ function prepareHardhatConfigs() {
         accounts: {
           mnemonic: getMnenomic(),
         },
-      }
+      };
 
       if (network === 'mainnet') {
-        acc[network].gasPrice = mainnetGwei * 1000000000
+        acc[network].gasPrice = mainnetGwei * 1000000000;
       }
 
-      return acc
-    }, hardhatLocalConfig)
+      return acc;
+    }, hardhatLocalConfig);
   }
 
   // The hardhat config object will be returned.
   const config = {
     networks: prepareNetworkConfigs(),
     solidity: {
-      version: "0.8.10",
+      version: '0.8.10',
       settings: {
         optimizer: {
           enabled: true,
           runs: 200,
         },
-      }
+      },
+    },
+
+    // Remove console.log when deploying to public networks
+    preprocess: {
+      eachLine: removeConsoleLog(
+        (hre) => hre.network.name !== 'hardhat' && hre.network.name !== 'localhost'
+      ),
     },
 
     /**
@@ -98,32 +100,31 @@ function prepareHardhatConfigs() {
      * More here: https://www.npmjs.com/package/hardhat-gas-reporter
      */
     gasReporter: {
-      currency: "USD",
+      currency: 'USD',
       enabled: !!process.env.REPORT_GAS,
-    }
-  }
+    },
+  };
 
-  // Hardhat plugin for integration with Tenderly. 
+  // Hardhat plugin for integration with Tenderly.
   // This plugin adds `tenderly:verify` task and `tenderly:push` task to Hardhat.
   // To use this plugin, you will need to exec `tenderly login` first on the `tenderly-cli`
   // More here: https://www.npmjs.com/package/@tenderly/hardhat-tenderly
-  if (process.env.TENDERLY_PROJECT_ID &&
-    process.env.TENDERLY_USERNAME) {
+  if (process.env.TENDERLY_PROJECT_ID && process.env.TENDERLY_USERNAME) {
     config.tenderly = {
       project: process.env.TENDERLY_PROJECT_ID,
       username: process.env.TENDERLY_USERNAME,
-    }
+    };
   }
 
-  // Hardhat plugin for integration with Etherscan's contract verification service. 
+  // Hardhat plugin for integration with Etherscan's contract verification service.
   // Provides the verify task, which allows you to verify contracts through Etherscan's service.
   if (process.env.ETHERSCAN_API_KEY) {
     config.etherscan = {
       apiKey: process.env.ETHERSCAN_API_KEY,
-    }
+    };
   }
 
-  return config
+  return config;
 }
 
-module.exports = prepareHardhatConfigs()
+module.exports = prepareHardhatConfigs();
