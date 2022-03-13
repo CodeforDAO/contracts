c832ec928f9b502fa44d2d05d3f576202b65f5fd
guoyu <o.u.turing@gmail.com>
2022-03-14 03:05:13 +0900

    Improve interface

--- contracts/interfaces/IMembership.sol
+++ contracts/interfaces/IMembership.sol
@@ -2,5 +2,21 @@
 pragma solidity ^0.8.0;
 
 interface IMembership {
+    function mint(bytes32[] calldata proof) external;
+
     function investMint(address to) external returns (uint256);
+
+    function isInvestor(uint256 tokenId) external view returns (bool);
+
+    function updateWhitelist(bytes32 merkleTreeRoot_) external;
+
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+
+    function updateTokenURI(uint256 tokenId, string calldata dataURI) external;
+
+    function pause() external;
+
+    function unpause() external;
+
+    function setupGovernor() external;
 }
---
+++ contracts/interfaces/ITreasury.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {DataTypes} from '../libraries/DataTypes.sol';
+
+interface ITreasury {
+    function updateShareSplit(DataTypes.ShareSplit memory _shareSplit) external;
+
+    function vestingShare(uint256[] calldata tokenId, uint8[] calldata shareRatio) external;
+
+    function updateInvestmentSettings(DataTypes.InvestmentSettings memory settings) external;
+
+    function invest() external payable;
+
+    function investInERC20(address token) external;
+}
