5992078893f188daca0a40c389e2168850a22a1e
guoyu <o.u.turing@gmail.com>
2022-03-22 17:14:44 +0900

    Improve comments for module Payroll

--- contracts/modules/Payroll.sol
+++ contracts/modules/Payroll.sol
@@ -9,6 +9,10 @@ import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import {Module} from '../core/Module.sol';
 import {IMembership} from '../interfaces/IMembership.sol';
 
+/**
+ * @title Payroll Module
+ * @notice The Payroll module enables the payment of monthly, weekly, annual bonuses and dividends by managing the payroll list of DAO members. It provides a quick way to help managers create module proposals.
+ */
 contract Payroll is Module {
     using Strings for uint256;
     using Address for address payable;
@@ -54,11 +58,6 @@ contract Payroll is Module {
         uint256 delay
     ) Module('Payroll', 'Payroll Contract V1', membership, operators, delay) {}
 
-    function AddPayroll(uint256 memberId, PayrollDetail calldata payroll) public onlyOperator {
-        _payrolls[memberId][payroll.period].push(payroll);
-        emit PayrollAdded(memberId, payroll);
-    }
-
     function GetPayroll(uint256 memberId, PayrollPeriod period)
         public
         view
@@ -67,6 +66,19 @@ contract Payroll is Module {
         return _payrolls[memberId][period];
     }
 
+    /**
+     * @dev Add Payroll
+     * Add a compensation plan for a member
+     */
+    function AddPayroll(uint256 memberId, PayrollDetail calldata payroll) public onlyOperator {
+        _payrolls[memberId][payroll.period].push(payroll);
+        emit PayrollAdded(memberId, payroll);
+    }
+
+    /**
+     * @dev Schedule Payroll
+     * Adding a member's compensation proposal to the compensation cycle
+     */
     function schedulePayroll(uint256 memberId, PayrollPeriod period)
         public
         onlyOperator
@@ -110,11 +122,16 @@ contract Payroll is Module {
         emit Payrollscheduled(memberId, _proposalId);
     }
 
+    /**
+     * @dev Exec Transfer
+     * Hook method for payroll proposals
+     */
     function execTransfer(
         address payable account,
         address[] calldata tokens,
         uint256[] calldata amounts
     ) external payable onlyTimelock {
+        // TODO: check balance and trigger `pullPayments` in module core
         if (msg.value > 0) {
             account.sendValue(msg.value);
         }
