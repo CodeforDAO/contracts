5ee46d712ba4e6d522977c256410ba6516b1f260
guoyu <o.u.turing@gmail.com>
2022-03-23 02:26:41 +0900

    Improve interface IModule

--- contracts/interfaces/IModule.sol
+++ contracts/interfaces/IModule.sol
@@ -2,7 +2,30 @@
 pragma solidity ^0.8.0;
 
 interface IModule {
+    function NAME() external returns (string memory);
+
+    function DESCRIPTION() external returns (string memory);
+
     function membership() external returns (address);
 
+    function share() external returns (address);
+
     function timelock() external returns (address);
+
+    function listOperators() external view returns (uint256[] memory);
+
+    function propose(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        string memory description
+    ) external returns (bytes32 id);
+
+    function confirm(bytes32 id) external;
+
+    function schedule(bytes32 id) external;
+
+    function excute(bytes32 id) external;
+
+    function cancel(bytes32 id) external;
 }
