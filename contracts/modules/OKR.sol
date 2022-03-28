54e4b405367e34558e771e34e33fdfc0e3c1dfa0
guoyu <o.u.turing@gmail.com>
2022-03-29 02:45:07 +0900

    Add description to custom modules

--- contracts/modules/OKR.sol
+++ contracts/modules/OKR.sol
@@ -3,4 +3,7 @@ pragma solidity ^0.8.0;
 
 import 'hardhat/console.sol';
 
-contract OKR {}
+/// @dev This module is designed to allow all members of the DAO to set their own OKRs and receive predetermined shares or compensation rewards based on the achievement of the corresponding goals.
+contract OKR {
+
+}
--- contracts/modules/Roadmap.sol
+++ contracts/modules/Roadmap.sol
@@ -3,4 +3,7 @@ pragma solidity ^0.8.0;
 
 import 'hardhat/console.sol';
 
-contract Roadmap {}
+/// @dev This module is designed to automate the distribution of shares or tokens at each stage through the completion of a pre-defined Roadmap.
+contract Roadmap {
+
+}
