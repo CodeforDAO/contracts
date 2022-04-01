2987c0e5332f21e61c205dc9172b931da91a6ebd
guoyu <o.u.turing@gmail.com>
2022-04-02 02:30:18 +0900

    Add contract Multicall to deployment mock

--- deploy/mocks.js
+++ deploy/mocks.js
@@ -1,12 +1,14 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-
-  await deploy('CallReceiverMock', {
+  const deployConfig = {
     from: deployer,
     args: [],
     log: true,
-  });
+  };
+
+  await deploy('CallReceiverMock', deployConfig);
+  await deploy('Multicall', deployConfig);
 };
 
 module.exports.tags = ['Mocks'];
