b730a04a752c9f7c0532be4a6797eb2fa5e737d3
guoyu <o.u.turing@gmail.com>
2022-04-14 04:35:36 +0900

    Add TEST_STAGE support for deploy scripts

--- deploy/membership.js
+++ deploy/membership.js
@@ -1,14 +1,26 @@
+const { ethers } = require('hardhat');
 const { testArgs } = require('../utils/configs');
+const { setupProof, membershipMintAndDelegate, isLocalhost } = require('../utils/helpers');
 
-module.exports = async ({ getNamedAccounts, deployments }) => {
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
 
   await deploy('Membership', {
     from: deployer,
     args: testArgs(),
     log: true,
   });
+
+  if (isLocalhost(chainId)) {
+    const deps = {};
+    await setupProof(deps);
+
+    if (process.env.TEST_STAGE === 'MINT_READY') {
+      await membershipMintAndDelegate(await ethers.getContract('Membership'), deps);
+    }
+  }
 };
 
 module.exports.tags = ['Membership'];
