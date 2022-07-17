b28f9241319eed429d0d4b479d4a5da1d29e5b63
Shun Kakinoki <shunkakinoki@gmail.com>
2022-07-16 17:18:45 -0700

    chore

--- tests/foundry/utils/helper.t.sol
+++ tests/foundry/utils/helper.t.sol
@@ -1,1 +1,25 @@
-contract Helper {}
+// SPDX-License-Identifier: MIT
+
+import 'forge-std/Test.sol';
+import 'murky/Merkle.sol';
+
+contract Helper is Test {
+    function setUpProof(uint256 i) public {
+        Merkle m = new Merkle();
+        bytes32[] memory data = new bytes32[](i);
+        bytes32 merkleRoot;
+        bytes32[] memory merkleProof;
+
+        for (uint256 j = 0; j < i; j++) {
+            data[j] = bytes32(uint256(j));
+        }
+
+        merkleRoot = m.getRoot(data);
+        merkleRoot = m.getRoot(data);
+        merkleProof = m.getProof(data, 2);
+    }
+
+    function testSetUpProof() public {
+        setUpProof(4);
+    }
+}
