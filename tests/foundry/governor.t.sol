cd044c1d1c7867c7af4d1e4923ad1938c21d8de4
Shun Kakinoki <shunkakinoki@gmail.com>
2022-07-23 18:58:19 -0700

    perf: ini git contracts

--- tests/foundry/governor.t.sol
+++ tests/foundry/governor.t.sol
@@ -1,30 +1,52 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.12;
 
 import 'forge-std/Test.sol';
 import '../../contracts/core/Governor.sol';
 import '../../contracts/core/Membership.sol';
 import '../../contracts/core/Share.sol';
+import '../../contracts/core/Treasury.sol';
 import {DataTypes} from '../../contracts/libraries/DataTypes.sol';
 
 contract GovernorTest is Test {
     Share share;
+    Treasury treasury;
     Membership membership;
-    TreasuryGovernor governor;
+    TreasuryGovernor membershipGovernor;
+    TreasuryGovernor shareGovernor;
 
     function setUp() public {
-        share = new Share('CodeforDAOShare', 'CFD');
         membership = new Membership(
             DataTypes.BaseToken({name: 'CodeforDAO', symbol: 'CODE'}),
             'https://codefordao.org/member/',
             'https://codefordao.org/membership/'
         );
-        governor = new TreasuryGovernor(
-            'CodeforDAO',
-            address(0),
-            Treasury(payable(address(0))),
+        share = new Share('CodeforDAOShare', 'CFD');
+        treasury = new Treasury(
+            1,
+            address(membership),
+            address(share),
+            DataTypes.InvestmentSettings(
+                true,
+                1,
+                2,
+                new address[](0),
+                new uint256[](0),
+                new uint256[](0)
+            )
+        );
+        membershipGovernor = new TreasuryGovernor(
+            string.concat(membership.name(), '-MembershipGovernor'),
+            address(membership),
+            treasury,
             DataTypes.GovernorSettings(0, 2, 3, 1)
+        );
+        shareGovernor = new TreasuryGovernor(
+            string.concat(membership.name(), '-ShareGovernor'),
+            address(share),
+            treasury,
+            DataTypes.GovernorSettings(1000, 10000, 4, 100)
         );
     }
 
