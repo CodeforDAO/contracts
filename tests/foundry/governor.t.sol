5c059140c5678fb478aff4d0c4d24c27ddc6bb68
Shun Kakinoki <shunkakinoki@gmail.com>
2022-07-23 19:32:51 -0700

    chore: update

--- tests/foundry/governor.t.sol
+++ tests/foundry/governor.t.sol
@@ -19,11 +19,17 @@ contract GovernorTest is Helpers {
         assertEq(share.symbol(), 'CFD');
     }
 
+    // membership.js deployment check
     function testMembershipDeploymentCheck() public {
         // Should bind a membership governor (1/1) contract
         assertEq(membership.governor(), address(membershipGovernor));
         // Should bind a share token (ERC20) contract
         assertEq(membership.shareToken(), address(share));
         assertEq(share.name(), 'CodeforDAOShare');
+        assertEq(share.symbol(), 'CFD');
+        // Should bind a share governor (ERC20 IVotes) contract
+        assertEq(membership.shareGovernor(), address(shareGovernor));
+        // Should bind a treasury (timelock) contract
+        assertEq(membershipGovernor.timelock(), address(treasury));
     }
 }
