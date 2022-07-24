43017f6790f6434aa4b51bc62e8bf66fd62b17ea
Shun Kakinoki <shunkakinoki@gmail.com>
2022-07-23 19:55:56 -0700

    chore: update

--- tests/foundry/governor.t.sol
+++ tests/foundry/governor.t.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.12;
 
 import './utils/helpers.t.sol';
+import '../../contracts/core/Membership.sol';
 
 contract GovernorTest is Helpers {
     function setUp() public {
@@ -31,5 +32,42 @@ contract GovernorTest is Helpers {
         assertEq(membership.shareGovernor(), address(shareGovernor));
         // Should bind a treasury (timelock) contract
         assertEq(membershipGovernor.timelock(), address(treasury));
+    }
+
+    // membership.js setup governor tests
+    function testMembershipSetupGovernor() public {
+        // Should not be able to call by a invaid account
+        vm.prank(address(0));
+        vm.expectRevert(
+            bytes(
+                'AccessControl: account 0x0000000000000000000000000000000000000000 is missing role 0x0000000000000000000000000000000000000000000000000000000000000000'
+            )
+        );
+        membership.setupGovernor(
+            address(membership),
+            address(membership),
+            address(membership),
+            address(membership)
+        );
+        // Should be able to setup the governor contract roles
+        assertEq(treasury.hasRole(keccak256('PROPOSER_ROLE'), address(membershipGovernor)), true);
+        assertEq(treasury.hasRole(keccak256('PROPOSER_ROLE'), address(shareGovernor)), true);
+        assertEq(treasury.hasRole(keccak256('EXECUTOR_ROLE'), address(0)), true);
+        // The init timelock admin role should be set to false
+        assertEq(treasury.hasRole(keccak256('TIMELOCK_ADMIN_ROLE'), address(membership)), false);
+        // Make sure the share token has right roles
+        assertEq(share.hasRole(keccak256('DEFAULT_ADMIN_ROLE'), address(treasury)), true);
+        assertEq(share.hasRole(keccak256('MINTER_ROLE'), address(treasury)), true);
+        assertEq(share.hasRole(keccak256('PAUSER_ROLE'), address(treasury)), true);
+        assertEq(share.hasRole(keccak256('DEFAULT_ADMIN_ROLE'), address(membership)), false);
+        assertEq(share.hasRole(keccak256('MINTER_ROLE'), address(membership)), false);
+        assertEq(share.hasRole(keccak256('PAUSER_ROLE'), address(membership)), false);
+        // Make sure initialSupply is minted
+        assertEq(share.balanceOf(address(treasury)), initialSupply);
+        // Make sure token transfer is paused by default
+        assertEq(share.paused(), enableMembershipTransfer);
+        // Deployer's role should be revoked
+        assertEq(membership.hasRole(keccak256('PAUSER_ROLE'), address(deployer)), false);
+        assertEq(membership.hasRole(keccak256('DEFAULT_ADMIN_ROLE'), address(deployer)), false);
     }
 }
