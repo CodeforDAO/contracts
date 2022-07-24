ab5231aed941eca82cba780f975f1ce30a5a9553
Shun Kakinoki <shunkakinoki@gmail.com>
2022-07-23 19:29:21 -0700

    perf: ini git contracts

--- tests/foundry/governor.t.sol
+++ tests/foundry/governor.t.sol
@@ -2,89 +2,28 @@
 
 pragma solidity ^0.8.12;
 
-import 'forge-std/Test.sol';
-import '../../contracts/core/Governor.sol';
-import '../../contracts/core/Membership.sol';
-import '../../contracts/core/Share.sol';
-import '../../contracts/core/Treasury.sol';
-import {DataTypes} from '../../contracts/libraries/DataTypes.sol';
+import './utils/helpers.t.sol';
 
-contract GovernorTest is Test {
-    address deployer;
-    bool enableMembershipTransfer = false;
-    uint256 initialSupply;
-    Share share;
-    Treasury treasury;
-    Membership membership;
-    TreasuryGovernor membershipGovernor;
-    TreasuryGovernor shareGovernor;
-
+contract GovernorTest is Helpers {
     function setUp() public {
-        deployer = msg.sender;
+        setUpProof(3);
+        contractsReady();
+    }
 
-        membership = new Membership(
-            DataTypes.BaseToken({name: 'CodeforDAO', symbol: 'CODE'}),
-            'https://codefordao.org/member/',
-            'https://codefordao.org/membership/'
-        );
-        share = new Share('CodeforDAOShare', 'CFD');
-        treasury = new Treasury(
-            1,
-            address(membership),
-            address(share),
-            DataTypes.InvestmentSettings(
-                true,
-                1,
-                2,
-                new address[](0),
-                new uint256[](0),
-                new uint256[](0)
-            )
-        );
-        if (initialSupply > 0) {
-            share.mint(address(treasury), 1_000_000);
-            treasury.updateShareSplit(DataTypes.ShareSplit(20, 10, 30, 40));
-        }
-        membership.grantRole(keccak256('DEFAULT_ADMIN_ROLE'), address(treasury));
-
-        share.grantRole(keccak256('DEFAULT_ADMIN_ROLE'), address(treasury));
-        share.grantRole(keccak256('MINTER_ROLE'), address(treasury));
-        share.grantRole(keccak256('PAUSER_ROLE'), address(treasury));
-        share.revokeRole(keccak256('DEFAULT_ADMIN_ROLE'), deployer);
-        share.revokeRole(keccak256('MINTER_ROLE'), deployer);
-        share.revokeRole(keccak256('PAUSER_ROLE'), deployer);
-
-        // All membership NFT is set to be non-transferable by default
-        if (!enableMembershipTransfer) {
-            membership.pause();
-        }
-        membership.revokeRole(keccak256('PAUSER_ROLE'), deployer);
-
-        membershipGovernor = new TreasuryGovernor(
-            string.concat(membership.name(), '-MembershipGovernor'),
-            address(membership),
-            treasury,
-            DataTypes.GovernorSettings(0, 2, 3, 1)
-        );
-        shareGovernor = new TreasuryGovernor(
-            string.concat(membership.name(), '-ShareGovernor'),
-            address(share),
-            treasury,
-            DataTypes.GovernorSettings(1000, 10000, 4, 100)
-        );
-        treasury.grantRole(keccak256('PROPOSER_ROLE'), address(membershipGovernor));
-        treasury.grantRole(keccak256('PROPOSER_ROLE'), address(shareGovernor));
-        membership.setupGovernor(
-            address(share),
-            address(treasury),
-            address(membershipGovernor),
-            address(shareGovernor)
-        );
-        membership.revokeRole(keccak256('DEFAULT_ADMIN_ROLE'), deployer);
+    function testSetUpProof() public {
+        setUpProof(4);
     }
 
     function testShare() public {
         assertEq(share.name(), 'CodeforDAOShare');
         assertEq(share.symbol(), 'CFD');
+    }
+
+    function testMembershipDeploymentCheck() public {
+        // Should bind a membership governor (1/1) contract
+        assertEq(membership.governor(), address(membershipGovernor));
+        // Should bind a share token (ERC20) contract
+        assertEq(membership.shareToken(), address(share));
+        assertEq(share.name(), 'CodeforDAOShare');
     }
 }
--- tests/foundry/utils/helpers.t.sol
+++ tests/foundry/utils/helpers.t.sol
@@ -4,8 +4,22 @@ pragma solidity ^0.8.0;
 
 import 'forge-std/Test.sol';
 import 'murky/Merkle.sol';
+import '../../../contracts/core/Governor.sol';
+import '../../../contracts/core/Membership.sol';
+import '../../../contracts/core/Share.sol';
+import '../../../contracts/core/Treasury.sol';
+import {DataTypes} from '../../../contracts/libraries/DataTypes.sol';
 
-contract Helper is Test {
+contract Helpers is Test {
+    address deployer;
+    bool enableMembershipTransfer = false;
+    uint256 initialSupply;
+    Share share;
+    Treasury treasury;
+    Membership membership;
+    TreasuryGovernor membershipGovernor;
+    TreasuryGovernor shareGovernor;
+
     function setUpProof(uint256 i) public {
         Merkle m = new Merkle();
         bytes32[] memory data = new bytes32[](i);
@@ -21,7 +35,67 @@ contract Helper is Test {
         merkleProof = m.getProof(data, 2);
     }
 
-    function testSetUpProof() public {
-        setUpProof(4);
+    function contractsReady() public {
+        deployer = msg.sender;
+
+        membership = new Membership(
+            DataTypes.BaseToken({name: 'CodeforDAO', symbol: 'CODE'}),
+            'https://codefordao.org/member/',
+            'https://codefordao.org/membership/'
+        );
+        share = new Share('CodeforDAOShare', 'CFD');
+        treasury = new Treasury(
+            1,
+            address(membership),
+            address(share),
+            DataTypes.InvestmentSettings(
+                true,
+                1,
+                2,
+                new address[](0),
+                new uint256[](0),
+                new uint256[](0)
+            )
+        );
+        if (initialSupply > 0) {
+            share.mint(address(treasury), 1_000_000);
+            treasury.updateShareSplit(DataTypes.ShareSplit(20, 10, 30, 40));
+        }
+        membership.grantRole(keccak256('DEFAULT_ADMIN_ROLE'), address(treasury));
+
+        share.grantRole(keccak256('DEFAULT_ADMIN_ROLE'), address(treasury));
+        share.grantRole(keccak256('MINTER_ROLE'), address(treasury));
+        share.grantRole(keccak256('PAUSER_ROLE'), address(treasury));
+        share.revokeRole(keccak256('DEFAULT_ADMIN_ROLE'), deployer);
+        share.revokeRole(keccak256('MINTER_ROLE'), deployer);
+        share.revokeRole(keccak256('PAUSER_ROLE'), deployer);
+
+        // All membership NFT is set to be non-transferable by default
+        if (!enableMembershipTransfer) {
+            membership.pause();
+        }
+        membership.revokeRole(keccak256('PAUSER_ROLE'), deployer);
+
+        membershipGovernor = new TreasuryGovernor(
+            string.concat(membership.name(), '-MembershipGovernor'),
+            address(membership),
+            treasury,
+            DataTypes.GovernorSettings(0, 2, 3, 1)
+        );
+        shareGovernor = new TreasuryGovernor(
+            string.concat(membership.name(), '-ShareGovernor'),
+            address(share),
+            treasury,
+            DataTypes.GovernorSettings(1000, 10000, 4, 100)
+        );
+        treasury.grantRole(keccak256('PROPOSER_ROLE'), address(membershipGovernor));
+        treasury.grantRole(keccak256('PROPOSER_ROLE'), address(shareGovernor));
+        membership.setupGovernor(
+            address(share),
+            address(treasury),
+            address(membershipGovernor),
+            address(shareGovernor)
+        );
+        membership.revokeRole(keccak256('DEFAULT_ADMIN_ROLE'), deployer);
     }
 }
