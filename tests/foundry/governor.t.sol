9501e3d3a3fb1f19ffa5a03e9480ff2916784403
Shun Kakinoki <shunkakinoki@gmail.com>
2022-07-29 07:27:14 -0700

    chore: update

--- tests/foundry/governor.t.sol
+++ tests/foundry/governor.t.sol
@@ -7,6 +7,13 @@ import '../../contracts/core/Governor.sol';
 import 'forge-std/console2.sol';
 
 contract GovernorTest is Helpers {
+    address[] internal targets = new address[](1);
+    uint256[] internal values = new uint256[](1);
+    bytes[] internal calldatas = new bytes[](1);
+    string[] internal signatures = new string[](1);
+    uint256 internal proposalId =
+        73267620643934072697764220838761558643702153097420137858599584192610788443557;
+
     event ProposalCreated(
         uint256 proposalId,
         address proposer,
@@ -18,35 +25,57 @@ contract GovernorTest is Helpers {
         uint256 endBlock,
         string description
     );
+    event VoteCast(
+        address indexed voter,
+        uint256 proposalId,
+        uint8 support,
+        uint256 weight,
+        string reason
+    );
 
     function setUp() public {
         setUpProof();
         contractsReady();
         membershipMintAndDelegate();
-    }
 
-    function testMembershipGovernor() public {
-        console2.log(address(this));
-    }
-
-    // governor.js #propose
-    function testMembershipGovernorPropose() public {
-        address[] memory targets = new address[](1);
         targets[0] = address(0);
-        uint256[] memory values = new uint256[](1);
         values[0] = uint256(0);
-        bytes[] memory calldatas = new bytes[](1);
         calldatas[0] = abi.encodeWithSignature('mockFunction()');
-        string[] memory signatures = new string[](1);
         signatures[0] = '';
 
         vm.roll(block.number + 1);
         assertEq(membershipGovernor.getVotes(deployer, block.number - 1), 1);
+    }
+
+    // governor.js #propose
+    function testMembershipGovernorPropose() public {
+        _voteOnProposal();
+
+        // Should not able to make a valid propose if user do not hold a NFT membership'
+        vm.prank(address(0));
+        vm.expectRevert(bytes('Governor: proposer votes below proposal threshold'));
+        membershipGovernor.propose(targets, values, calldatas, '<proposal description>');
+    }
+
+    // governor.js #vote
+    function testMembershipGovernorVote() public {
+        // Should able to cast votes on a valid proposal
+        _voteOnProposal();
+
+        vm.roll(block.number + 1);
 
         vm.prank(deployer);
         vm.expectEmit(true, true, true, true);
+        emit VoteCast(deployer, proposalId, 1, 1, '');
+        membershipGovernor.castVote(proposalId, 1);
+        assertEq(membershipGovernor.hasVoted(proposalId, deployer), true);
+    }
+
+    function _voteOnProposal() internal {
+        vm.prank(deployer);
+        vm.expectEmit(true, true, true, true);
         emit ProposalCreated(
-            73267620643934072697764220838761558643702153097420137858599584192610788443557,
+            proposalId,
             deployer,
             targets,
             values,
@@ -56,11 +85,6 @@ contract GovernorTest is Helpers {
             4,
             '<proposal description>'
         );
-        membershipGovernor.propose(targets, values, calldatas, '<proposal description>');
-
-        // Should not able to make a valid propose if user do not hold a NFT membership'
-        vm.prank(address(0));
-        vm.expectRevert(bytes('Governor: proposer votes below proposal threshold'));
         membershipGovernor.propose(targets, values, calldatas, '<proposal description>');
     }
 }
